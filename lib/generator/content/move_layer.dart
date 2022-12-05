4286a14c6c268d9253fc6d4cea8176b70a10c48c
licky <895260021@qq.com>
2022-12-05 16:21:07 +0800

    move_layer.dart 添加事件控制

--- lib/generator/content/move_layer.dart
+++ lib/generator/content/move_layer.dart
@@ -3,8 +3,23 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 
 class MoveLayer extends HookWidget {
   final Widget child;
+  final VoidCallback? moveUp;
+  final VoidCallback? moveDown;
+  final VoidCallback? moveLeft;
+  final VoidCallback? moveRight;
+  final VoidCallback? copy;
+  final VoidCallback? delete;
 
-  const MoveLayer({Key? key, required this.child}) : super(key: key);
+  const MoveLayer({
+    Key? key,
+    required this.child,
+    this.moveUp,
+    this.moveDown,
+    this.moveLeft,
+    this.moveRight,
+    this.copy,
+    this.delete,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +29,7 @@ class MoveLayer extends HookWidget {
         child: ElevatedButton(
           onPressed: onPressed,
           style: ElevatedButton.styleFrom(
-            shape: CircleBorder(),
+            shape: const CircleBorder(),
             padding: EdgeInsets.zero,
             backgroundColor: Colors.white,
             foregroundColor: Colors.black,
@@ -24,36 +39,58 @@ class MoveLayer extends HookWidget {
       );
     }
 
+    renderIfNotNull<T>(T? nullable, Widget Function(T) item) {
+      if (nullable == null) {
+        return const SizedBox();
+      }
+      return item.call(nullable);
+    }
+
     return Stack(
       children: [
-        Opacity(opacity: 0.2,child: Container(color: Colors.blueAccent,width: 800,height: 100,),),
+        Opacity(
+          opacity: 0.2,
+          child: Container(color: Colors.blueAccent, width: 800, height: 100),
+        ),
         child,
         SizedBox(
           height: 100,
           width: 800,
           child: Column(
             children: [
-              iconButton(Icons.arrow_upward, () {}),
+              renderIfNotNull(
+                  moveUp, (action) => iconButton(Icons.arrow_upward, action)),
               Expanded(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    iconButton(Icons.switch_left, () {}),
-                    iconButton(Icons.switch_right, () {}),
+                    renderIfNotNull(moveLeft,
+                        (action) => iconButton(Icons.switch_left, action)),
+                    renderIfNotNull(moveRight,
+                        (action) => iconButton(Icons.switch_right, action)),
                   ],
                 ),
               ),
               Stack(
                 alignment: Alignment.center,
                 children: [
-                  iconButton(Icons.arrow_downward, () {}),
+                  renderIfNotNull(
+                    moveDown,
+                    (action) => iconButton(Icons.arrow_downward, action),
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      iconButton(Icons.copy, () {}),
+                      renderIfNotNull(
+                        copy,
+                        (action) => iconButton(Icons.copy, action),
+                      ),
                       const SizedBox(width: 8),
-                      iconButton(Icons.delete, () {}),
+                      renderIfNotNull(
+                        delete,
+                        (action) => iconButton(Icons.delete, action),
+                      ),
                       const SizedBox(width: 16),
                     ],
                   ),
