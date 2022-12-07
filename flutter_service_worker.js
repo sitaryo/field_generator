84258c23a12ccc5376e71092ce4a9a3727c3276f
licky <895260021@qq.com>
2022-12-07 09:47:15 +0800

    build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "afdeaf5a0691faace7cb4fc9962729fe",
-"index.html": "a9e6d288f30c24883fb72600027b26a3",
-"/": "a9e6d288f30c24883fb72600027b26a3",
-"main.dart.js": "59e4fbd079ae5c81d5e1e648935b32ec",
+"index.html": "92f998be0f2d0fb116848ab4566a5e17",
+"/": "92f998be0f2d0fb116848ab4566a5e17",
+"main.dart.js": "031fb56e1648e4a33165d3af1f19b290",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
